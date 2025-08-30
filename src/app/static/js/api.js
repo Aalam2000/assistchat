41528b0194573dd2aeb514b3af028659e3043d4e
Aalam2000 <a9160371537@gmail.com>
2025-08-30 22:45:31 +0400

    7 Настройка регистрации ГУГЛ

--- src/app/static/js/api.js
+++ src/app/static/js/api.js
@@ -1,6 +1,7 @@
 // src/app/static/js/api.js
 async function apiPost(url, data) {
   const r = await fetch(url, {
+    credentials: "same-origin",
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data || {})
