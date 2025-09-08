f451a6e0cc2c92c316dee804c70106381159a925
Aalam2000 <a9160371537@gmail.com>
2025-09-08 10:55:26 +0400

    2 Расширение сервиса - новые таблицы

--- src/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/models/__init__.py
-from src.common.db import Base
-from .user import User, RoleEnum
-from .tg_account import TgAccount
-from .message import Message
-# если есть еще модели (updates_seen и пр.), тоже импортируем
