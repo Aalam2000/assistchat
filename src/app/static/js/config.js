0d7470701dc630a09ff8d6dbd8f7db597f08032b
Aalam2000 <a9160371537@gmail.com>
2025-09-24 12:49:48 +0400

    Обновленные стили 2

--- src/app/static/js/config.js
+++ src/app/static/js/config.js
@@ -7,3 +7,12 @@ window.APP_CONFIG = {
     me: "/api/auth/me",
   }
 };
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("i18n-toggle");
+  const waitModal = document.getElementById("lang-wait");
+  if (btn && waitModal) {
+    btn.addEventListener("click", () => {
+      waitModal.classList.remove("hidden");
+    });
+  }
+});
